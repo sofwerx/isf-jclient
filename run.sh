0581aff0ad9b26631c74d88d0f2edf1c63c83fcb
Ian Blenke <ian@blenke.com>
2018-02-14 10:45:02 -0500

    Iterating on dockerization

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+set -ex
+
+NODE_LIST="${NODE_LIST:-http://172.18.0.1:14265}"
 
 cat <<EOF > config.ini
 [nodes]
 sync_check_interval = ${SYNC_CHECK_INTERVAL:-600}
-node_list = ${NODE_LIST:-http://192.168.29.253:19265}
+node_list = ${NODE_LIST}
 third_party_node_list = ${THIRD_PARTY_NODE_LIST:-true}
 
 [log]
@@ -18,5 +21,7 @@ password = ${PASSWORD}
 amount = ${THREADS_AMOUNT:-1}
 priority = ${THREADS_PRIORITY:-2}
 EOF
+
+curl ${NODE_LIST}
 
 exec java -jar target/isf-jclient-1.0.3.jar start
